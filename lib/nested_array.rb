3366473c384450457d0698f760fbab4ba0d4c4bc
John Easter <john@owltestprep.com>
2020-07-15 13:29:49 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,7 +17,7 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
-  PRODUCE = [["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"],
+  assembled_matrix = [["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"],
   ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]]
   # Build an array that contains both of the above arrays
   # This matrix will represent a produce storage room
@@ -26,7 +26,7 @@ def assembled_matrix
 end
 
 def sorted_matrix
-  PRODUCE = [["Eggplant", "Grapefruit", "Oranges", "Pineapple", "Watermelon"],
+  sorted_matrix = [["Eggplant", "Grapefruit", "Oranges", "Pineapple", "Watermelon"],
   ["Asparagus", "Avocadoes", "Grapes", "Potatoes", "Strawberries"]]
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
